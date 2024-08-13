fe251e0cff428dfe5c4fb7f830c01d7f3f871beb
NAME-NikhilPatil <nikhilpatilhaha@gmail.com>
2024-08-13 21:45:16 +0530

    SQL_basics

---
+++ sub_query(where).sql
@@ -0,0 +1,27 @@
+create database college;
+use college;
+
+create table student(
+ rollno int primary key,
+ name varchar(50),
+ marks int not null,
+ grade varchar(1),
+ city varchar(20)
+ );
+
+insert into student(rollno,name,marks,grade,city) 
+ values
+ (101, "anil", 78, "C", "Pune"),
+(102, "bhumika", 93, "A", "Mumbai"),
+(103, "chetan", 85, "B", "Mumbai"),
+(104, "dhruv", 96, "A", "Delhi"),
+(105, "emanuel", 92, "F", "Delhi"),
+(106, "farah", 82, "B", "Delhi");
+
+select name,rollno
+from student
+where rollno in (
+ select rollno
+ from student
+ where rollno%2=0
+ );
